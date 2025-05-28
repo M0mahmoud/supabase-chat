41d3b75634091e263dd3cc0201b4ba2e5b2446cb
Mahmoud Mohamed <88073988+M0mahmoud@users.noreply.github.com>
2025-05-28 21:13:35 +0300

    feat: Adding NextTopLoader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import Sidebar from "@/components/layout/sidebar/Sidebar";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
         className={`${geistSans.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="dark">
+          <NextTopLoader color="#155dfc" height={4} showSpinner={false} />
           <div className="flex w-full h-full min-h-dvh max-w-[1660px] mx-auto">
             <Sidebar />
             <div className="flex justify-center items-center w-full h-full">
